58da2da703ab9c329085cfb19e462c0a1abda20e
Mayur Paralkar <mayurparalkar@MayurASUS.localdomain>
2017-06-19 16:02:12 -0700

    remove redundant file vcfParseCode.py

--- vcfParseCode.py
+++
@@ -1,9 +0,0 @@
-#Parse vcf by creating set of tuples; form: (chrom, pos, ref, alt)
-def altParseVCF(vcfStr):
-	allMuts = list()
-	for line in vcfStr.splitlines():
-		if((len(line) < 1) or (not line[0].isdigit())):
-			continue
-		vals = line.strip().split('\t')
-		allMuts.append((vals[0], vals[1], vals[3], vals[4]))
-	return allMuts
