3b73d0f0573bf7bfd1f80792b688be01c8c35b94
Mihir Paralkar <mihirparalkar@Twindian.localdomain>
2017-06-21 10:10:13 -0700

    include code for kmer

--- tempTestVcf.py
+++ tempTestVcf.py
@@ -2,7 +2,58 @@ import bisect
 import argparse
 import bowtie_index
 import sys
+import math
 #import pickle to unpickle ordered_exon_dict
+
+codon_table = {"TTT":"F", "TTC":"F", "TTA":"L", "TTG":"L",
+    "TCT":"S", "TCC":"S", "TCA":"S", "TCG":"S",
+    "TAT":"Y", "TAC":"Y", "TAA":"Stop", "TAG":"Stop",
+    "TGT":"C", "TGC":"C", "TGA":"Stop", "TGG":"W",
+    "CTT":"L", "CTC":"L", "CTA":"L", "CTG":"L",
+    "CCT":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAT":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGT":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "ATT":"I", "ATC":"I", "ATA":"I", "ATG":"M",
+    "ACT":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAT":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGT":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GTT":"V", "GTC":"V", "GTA":"V", "GTG":"V",
+    "GCT":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAT":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGT":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
+
+def turn_to_aa(nucleotide_string):
+    aa_string = ""
+    for aa in range(len(nucleotide_string)//3):
+        codon = codon_table[nucleotide_string[3*aa:3*aa+3]]
+        if (codon == "Stop"):
+            break
+        else:
+            aa_string += codon
+    return aa_string
+
+def my_print_function(kmer_list):
+    print("WILD TYPE" + "\t" + "MUTANT TYPE")
+    for wtmtPair in kmer_list:
+        wt,mt = wtmtPair
+        print(wt + "\t" + mt)
+    return None
+
+
+def kmer(normal_aa, mutated_aa = ""):
+    if (len(mutated_aa) == 0):
+        mutated_aa = normal_aa
+    kmer_list = list()
+    #Loop through window sizes
+    for ksize in range(8, 12):
+        for startIndex in range(len(mutated_aa)-ksize):
+            kmer_list.append((normal_aa[startIndex:startIndex+ksize], mutated_aa[startIndex:startIndex+ksize]))
+    final_list = list()
+    for WT,MT in kmer_list:
+        if (WT != MT):
+            final_list.append((WT, MT))
+    my_print_function(final_list)
+    return final_list
 
 def get_exons(transcript_id, mutation_pos, strand_length_left, 
               strand_length_right, exon_dict):
@@ -25,6 +76,8 @@ def get_exons(transcript_id, mutation_pos, strand_length_left,
     ordered_exon_dict = exon_dict
     if transcript_id not in ordered_exon_dict:
         return []
+    #Increase the strand length by 1 to account for mutation_pos collection
+    strand_length_left += 1
     total_strand_length = strand_length_right + strand_length_left
     original_length_left = strand_length_left
     exon_list = ordered_exon_dict[transcript_id]
@@ -34,8 +87,9 @@ def get_exons(transcript_id, mutation_pos, strand_length_left,
         middle_exon_index -= 2
     nucleotide_index_list = []
     curr_left_index = middle_exon_index
-    curr_right_index = middle_exon_index+1 #Exon boundary end indexes
-    curr_pos_left = mutation_pos
+    curr_right_index = middle_exon_index+1 #Exon boundary end indexes\
+    #Increase by one to ensure mutation_pos is collected into boundaries.
+    curr_pos_left = mutation_pos + 1
     curr_pos_right = mutation_pos #Actual number in chromosome
     #If the mutation is not on in exon bounds, return [].
     if (mutation_pos > exon_list[curr_right_index] or 
@@ -46,7 +100,11 @@ def get_exons(transcript_id, mutation_pos, strand_length_left,
           sum([index[1] for index in nucleotide_index_list]) 
           < (original_length_left)):
         if curr_pos_left-exon_list[curr_left_index] >= strand_length_left:
-            nucleotide_index_list.append((curr_pos_left-strand_length_left,
+            if curr_pos_left != mutation_pos+1:
+                nucleotide_index_list.append((curr_pos_left-strand_length_left+1,
+                                          strand_length_left))
+            else:
+                nucleotide_index_list.append((curr_pos_left-strand_length_left,
                                           strand_length_left))
             strand_length_left = 0
         else:
@@ -62,7 +120,8 @@ def get_exons(transcript_id, mutation_pos, strand_length_left,
                                       - strand_length_left
                                       + strand_length_right)
                 break
-    nucleotide_index_list.append((mutation_pos, 1))
+    #Reverse list to get tuples in order
+    nucleotide_index_list = list(reversed(nucleotide_index_list))
     while(len(nucleotide_index_list) == 0 or 
               sum([index[1] for index in nucleotide_index_list]) 
               < (total_strand_length)):
