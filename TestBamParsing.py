f294a93bce963b1a2b976fbfc9cfbccacffd2582
Austin <nguyenau@ohsu.edu>
2017-06-22 14:47:01 -0700

    Did some test values

--- TestBamParsing.py
+++ TestBamParsing.py
@@ -3,6 +3,13 @@
 #Description: Testing parsing capabilities of pysam and also the ease of extraction/modification
 
 import pysam
+#just some test variables
+start = 43384872
+end   = 43384951
+chrom = "NC_000017.10"
+pos   = 43384880
+mut   = "G"
+
 #reads in the input file as an Alignment file
 inputfile = pysam.AlignmentFile("testbam.bam", "rb") 
 
@@ -10,6 +17,12 @@ inputfile = pysam.AlignmentFile("testbam.bam", "rb")
 #read acts as an aligned segment
 #if we used data = inputfile..., data would be an iterator which has very little documentation
 #however, we can keep use "data" if we want to iterate through later.  Better modularization
-for read in inputfile.fetch("NC_000017.10", start = 43384873, end = 43384986): 
-    print read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence
 
+"""for read in inputfile.fetch("NC_000017.10", start = 43384873, end = 43384986): 
+    print read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence"""
+
+for read in inputfile.fetch(chrom, start = 43384873, end = 43384986):
+    if (read.reference_start < pos and read.reference_end > pos and read.query_sequence[pos-read.reference_start-1] == mut):
+        print read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence, pos-read.reference_start, read.query_sequence[pos-read.reference_start-1]
+
+    
