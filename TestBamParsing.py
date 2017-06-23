4ffb611433a1eded0471a9135e60478db0ee81a9
Austin <nguyenau@ohsu.edu>
2017-06-23 14:17:59 -0700

    takes input file and produces bam output

--- TestBamParsing.py
+++ TestBamParsing.py
@@ -13,11 +13,22 @@ paramfile = open("testinput.txt", "r")
 #reads in the input file as an Alignment file
 inputfile = pysam.AlignmentFile("testbam.bam", "rb") 
 
-def getbaminfo(inputfile, position, chromosome, mutation):
+#fetch returns the reads with any nucleotide within the range provided
+#read acts as an aligned segment
+#if we used data = inputfile..., data would be an iterator which has very little documentation
+#however, we can keep use "data" if we want to iterate through later.  Better modularization
+#Function: Gets info from bam file and returns needed vars
+#Inputs: the bam file, position of mutation, chromosome number, and the mutation base
+#Outputs: Just printing and writing filtered results to bam file
+#Description: Selects from bam using fetch, then matches the criteria of the correct base at the correct position
+def getbaminfo(header, inputfile, position, chromosome, mutation):
     for read in inputfile.fetch(chromosome, start = position, end = position+1):
         if (read.reference_start < position and read.reference_end > position and read.query_sequence[position-read.reference_start-1] == mutation):
-            print read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence, position-read.reference_start, read.query_sequence[position-read.reference_start-1]
-            #print read
+            print read.query_name, read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence, position-read.reference_start, read.query_sequence[position-read.reference_start-1]
+            with pysam.AlignmentFile("outputbam.bam", "wb", header=header) as outf:
+                a = pysam.AlignedSegment()
+                a = read
+                outf.write(a)
 
     return
 #Simple Parsing of the param file, we only need 3 attributes to make the fetch from pysam
@@ -27,14 +38,13 @@ for lines in paramfile:
     chromosome = parsedline[0]
     position   = int(parsedline[1])
     mutation   = parsedline[2]
-    getbaminfo(inputfile, position, chromosome, mutation)
+    header = { 'HD': {'VN': '1.0'},
+            'SQ': [{'LN': 1575, 'SN': 'chr1'},
+                   {'LN': 1584, 'SN': 'chr2'}] }
+    getbaminfo(header, inputfile, position, chromosome, mutation)
     #print chromosome, position, mutation
     
 
-#fetch returns the reads with any nucleotide within the range provided
-#read acts as an aligned segment
-#if we used data = inputfile..., data would be an iterator which has very little documentation
-#however, we can keep use "data" if we want to iterate through later.  Better modularization
 
 
 
