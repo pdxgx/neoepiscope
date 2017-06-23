4e21d8eea0758bca71775d65ae9e68d8b31979e4
Austin <nguyenau@ohsu.edu>
2017-06-23 10:20:36 -0700

    Did input file addition

--- TestBamParsing.py
+++ TestBamParsing.py
@@ -4,25 +4,38 @@
 
 import pysam
 #just some test variables
-start = 43384872
-end   = 43384951
-chrom = "NC_000017.10"
-pos   = 43384880
-mut   = "G"
+'''chromosome = "NC_000017.10"
+position   = 43384880
+mutation   = "G"'''
+
+paramfile = open("testinput.txt", "r")
 
 #reads in the input file as an Alignment file
 inputfile = pysam.AlignmentFile("testbam.bam", "rb") 
+
+def getbaminfo(inputfile, position, chromosome, mutation):
+    for read in inputfile.fetch(chromosome, start = position, end = position+1):
+        if (read.reference_start < position and read.reference_end > position and read.query_sequence[position-read.reference_start-1] == mutation):
+            print read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence, position-read.reference_start, read.query_sequence[position-read.reference_start-1]
+            #print read
+
+    return
+#Simple Parsing of the param file, we only need 3 attributes to make the fetch from pysam
+
+for lines in paramfile:
+    parsedline = lines.strip().split()
+    chromosome = parsedline[0]
+    position   = int(parsedline[1])
+    mutation   = parsedline[2]
+    getbaminfo(inputfile, position, chromosome, mutation)
+    #print chromosome, position, mutation
+    
 
 #fetch returns the reads with any nucleotide within the range provided
 #read acts as an aligned segment
 #if we used data = inputfile..., data would be an iterator which has very little documentation
 #however, we can keep use "data" if we want to iterate through later.  Better modularization
 
-"""for read in inputfile.fetch("NC_000017.10", start = 43384873, end = 43384986): 
-    print read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence"""
 
-for read in inputfile.fetch(chrom, start = 43384873, end = 43384986):
-    if (read.reference_start < pos and read.reference_end > pos and read.query_sequence[pos-read.reference_start-1] == mut):
-        print read.reference_start, read.reference_end, read.query_alignment_length, read.query_sequence, pos-read.reference_start, read.query_sequence[pos-read.reference_start-1]
 
     
