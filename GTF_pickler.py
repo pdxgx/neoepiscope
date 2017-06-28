13b7b71c2a9317e29198efe148e0036c28842adf
Mayur Paralkar <mayurparalkar@MayurASUS.localdomain>
2017-06-28 11:16:15 -0700

    Fixes variable names with 'exon' to 'cds' to better reflect values

--- GTF_pickler.py
+++ GTF_pickler.py
@@ -14,31 +14,31 @@ args = parser.parse_args()
 
 my_file = open(args.gtf, "r") # ex: open("gencode.txt").read()
 
-exon_dict = {}
+cds_dict = {}
 orf_dict = {}
 for line in my_file:
     if not line or line[0] == '#': continue
     tokens = line.strip().split('\t')
-    if tokens[2] != "exon": continue
+    if tokens[2] != "CDS": continue
     read_info = tokens[8].split(';')
     version_in_id = read_info[1].find('.')
     if version_in_id == -1:
         transcript_id = read_info[1][16:]
     else:
         transcript_id = read_info[1][16:version_in_id]
-    if transcript_id not in exon_dict:
-        exon_dict[transcript_id] = [int(tokens[3]), int(tokens[4])]
+    if transcript_id not in cds_dict:
+        cds_dict[transcript_id] = [int(tokens[3]), int(tokens[4])]
     else:
-        insert_point = 2*bisect.bisect(exon_dict[transcript_id][0::2],
+        insert_point = 2*bisect.bisect(cds_dict[transcript_id][0::2],
                                        int(tokens[3]))
-        #I'm assuming there aren't two exons that start at the same point.
-        exon_dict[transcript_id] = (exon_dict[transcript_id][:insert_point] 
+        #I'm assuming there aren't two CDSs that start at the same point.
+        cds_dict[transcript_id] = (cds_dict[transcript_id][:insert_point] 
                                     + [int(tokens[3]), int(tokens[4])] 
-                                    + exon_dict[transcript_id][insert_point:])
+                                    + cds_dict[transcript_id][insert_point:])
     if transcript_id not in orf_dict:
         orf_dict[transcript_id] = (tokens[6])
     
 #@TODO: Don't forget to pickle the chrom_dict also!!!!!!!
 pickle_out = open(args.dump, "wb")
-pickle.dump([exon_dict, orf_dict], pickle_out)
+pickle.dump([cds_dict, orf_dict], pickle_out)
 pickle_out.close()
