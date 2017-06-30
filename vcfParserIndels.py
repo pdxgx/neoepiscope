65f2ceb2799620a2d0ba2f6c61cb767e1be17e88
Mihir Paralkar <mihirparalkar@Twindian.localdomain>
2017-06-30 12:37:37 -0700

    corrected for indel detection in stream

--- vcfParserIndels.py
+++ vcfParserIndels.py
@@ -264,7 +264,7 @@ try:
                 )
             tokens = info.strip().split('|')
             mute_type = tokens[1]
-            if(mute_type != "missense_variant" and mute_type != "GDel"): continue
+            if(mute_type != "missense_variant" and len(orig) == len(pos)): continue
             (trans_id, rel_pos) = (tokens[6], int(tokens[13]))
             if mute_type == "missense_variant":
                 pos_in_codon = (rel_pos+2)%3 #ie: ATG --> 0,1,2
@@ -273,7 +273,7 @@ try:
                     pos_in_codon = 2-pos_in_codon
             except:
                 continue
-            if mute_type == "GDel":
+            if len(orig) != len(pos):
                 cds_list = cds_dict[trans_id]
                 orf_list = orf_dict[trans_id]
                 orf_index = bisect.bisect(cds_list[0:1:2], pos)
@@ -283,7 +283,7 @@ try:
                 #This code doesn't work for deletions. Only insertions.
                 if strand == "+":
                     pos_in_codon = (pos - (cds_start-frame)%3)%3
-                    if len(orig) > len(alt):
+                    if len(alt) > len(orig):
                         right_query = 2-(pos_in_codon+len(orig)-len(alt))%3
                     else:
                         right_query = 2-pos_in_codon
