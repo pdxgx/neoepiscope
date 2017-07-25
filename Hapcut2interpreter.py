3d7cbed0e9d876afba13525bd99264c56a537b5a
Austin <nguyenau@ohsu.edu>
2017-07-25 10:10:18 -0700

    Updated getting frequencies

--- Hapcut2interpreter.py
+++ Hapcut2interpreter.py
@@ -3,6 +3,36 @@
 #Description: Returns phased segment of queried data.
 #Input: Takes in chromosome, pos:start-end, and reference/germline sequence
 
+        
+
+
+#Function: get freq
+#Inputs: vcf file
+#Output: the position of the allele frequency from the VCF 10th column (which pos)
+#Description: will read to header file and pattern match on the line
+
+#pattern match beginning of header file VCF
+#Note: check if all vcf have the the header file format
+#if not, we're in trouble....
+def getfreqlabel(vcffile):
+    vcf = open(vcffile, "r")
+    for read in vcf:
+        flag = 1
+        if 'allele frequency' in read.lower():
+                startchar = 11
+                freqlabel = ""
+                while read[startchar] != ',':
+                    freqlabel = freqlabel + read[startchar] 
+                    startchar += 1
+                #print freqlabel
+                flag = 0
+                break
+
+    if flag == 1:
+        print "Raise runtime error"
+    return freqlabel
+
+
 #Function: returnphasing
 #Inputs: string- chromsome#, int- start position, int- end position, string- reference sequence from either reference genome or germline
 #Output: 1 or 2 sequences that include variants, 1 if all variants are on same chromosome, 2 if variants are spread across both chromosome
@@ -11,7 +41,6 @@
 #Description: applies mutation to the relevant chromosome strand
 
 #Return flags: 0- mutation from Hapcut2 output, 1- mutation from vcf, 2- no mutation
-
 
 def returnphasing(chromosome, startpos, endpos, refseq, vcfname):
     chrome1 = list(refseq)
@@ -24,62 +53,97 @@ def returnphasing(chromosome, startpos, endpos, refseq, vcfname):
     chrome2count = 0
     chrome1vcflist = []
     chrome2vcflist = []
-    errorflag = 2
+    chromeflag1 = 0
+    chromeflag2 = 0
+    errorflag = 0
     hapcutfile = open("haplotype_output_file", "r")
     for line in hapcutfile:
         stripped = line.strip().split()
         if stripped[0] != "********":
             if stripped[3]  == chromosome and int(stripped[4]) <= endpos and int(stripped[4]) >= startpos:
-                errorflag = 0
                 #print stripped[3], stripped[1], stripped[2], chromosome, stripped[4], endpos, startpos
                 mutpos = int(stripped[4]) - startpos
                 if (stripped[1] == '-' or stripped[2] == '-'):
                     continue
                 if int(stripped[1]) != 0:
+                    chromeflag1 = 1
                     chrome1[mutpos] = stripped[6]
                     chrome1count = 1
                     chrome1vcflist.append(int(stripped[0]))
                     chromepassed1[mutpos] = 1
-                    hapscore1[mutpos] = stripped[10]
+                    hapscore1[mutpos] = float(stripped[10])
                 if int(stripped[2]) != 0:
+                    chromeflag2 = 1
                     chrome2[mutpos] = stripped[6]
                     chrome2count = 1
                     chrome2vcflist.append(int(stripped[0]))
                     chromepassed2[mutpos] = 1
-                    hapscore2[mutpos] = stripped[10]
+                    hapscore2[mutpos] = float(stripped[10])
 
     linecount = 0
     vcffile = open(vcfname, "r")
+    freqlabel = getfreqlabel(vcfname)
     for line in vcffile:
         #print line
         if not line or line[0] == '#': continue
         linecount += 1
+        infostring = []
         stripped = line.strip().split()
+        info = stripped[8].split(':')
+        allelefreqline = stripped[9].split(':')
         if stripped[0] == chromosome and int(stripped[1]) <= endpos and int(stripped[1]) >= startpos:
             mutpos = int(stripped[1]) - startpos
             if chromepassed1[mutpos] != 1:
+                chromeflag1 = 1
                 chrome1[mutpos] = stripped[4]
                 chrome1vcflist.append(linecount)
-                errorflag = 1
+                #errorflag = 1
                 chromepassed1[mutpos] = 2
+                counter = 0
+                for infos in info:
+                    if infos != freqlabel:
+                        counter += 1
+                    else:
+                         hapscore1[mutpos] = float(allelefreqline[counter].rstrip('%'))
             if chromepassed2[mutpos] != 1:
+                chromeflag2 = 1
                 chrome2[mutpos] = stripped[4]
                 chrome2vcflist.append(linecount)
-                errorflag = 1
+                #errorflag = 1
                 chromepassed2[mutpos] = 2
+                counter = 0
+                for infos in info:
+                    if infos != freqlabel:
+                        counter += 1
+                        #print infos
+                    else:
+                         #print stripped[8], mutpos, counter
+                         hapscore2[mutpos] = float(allelefreqline[counter].rstrip('%'))
+                         #print counter
+                         #print allelefreqline[counter]
+                         #print hapscore2[mutpos]
 
     vcffile.close()
                    
     hapcutfile.close()
     chrome1 = ''.join(chrome1)
     chrome2 = ''.join(chrome2)
-    if chrome1count == 1 and chrome2count == 1:
+    '''if chrome1count == 1 and chrome2count == 1:
         return (chrome1, chrome1vcflist, chrome2, chrome2vcflist, errorflag, chromepassed1, chromepassed2, hapscore1, hapscore2)
     if chrome1count == 1 and chrome2count == 0:
         return (chrome1, chrome1vcflist, errorflag, chromepassed1, hapscore1)
     if chrome1count == 0 and chrome2count == 1:
         return (chrome2, chrome2vcflist, errorflag, chromepassed2, hapscore2)
     else:
-        return (refseq,errorflag)
+        return (refseq,errorflag)'''
+    if chromeflag1 == 1 and chromeflag2 == 1:
+        return (chrome1, chrome1vcflist, chrome2, chrome2vcflist, errorflag, chromepassed1, chromepassed2, hapscore1, hapscore2)
+    if chromeflag1 == 1 and chromeflag2 == 0:
+        return (chrome1, chrome1vcflist, errorflag, chromepassed1, hapscore1)
+    if chromeflag1 == 0 and chromeflag2 == 1:
+        return (chrome2, chrome2vcflist, errorflag, chromepassed2, hapscore2)
+    else:
+        return (refseq, errorflag)
+
             
 
