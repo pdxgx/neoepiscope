7dd0b31a16d983743b8b8f66aae4a725456a67d1
reidt03 <thompsre@ohsu.edu>
2017-08-15 11:14:55 -0700

    missing +

--- src/combine_vcf.py
+++ src/combine_vcf.py
@@ -22,7 +22,7 @@ def combinevcf(vcf1, vcf2, outfile):
     header.close()
     command2 = "sort -k1,1 -k2,2n " + vcf2 + ".temp2 > " + vcf2 + ".sorted"
     os.system(command2)
-    command3 = "cat " + vcf2 + ".header " + vcf2 ".sorted > " + vcf2 + ".combine2"
+    command3 = "cat " + vcf2 + ".header " + vcf2 + ".sorted > " + vcf2 + ".combine2"
     os.system(command3)
     cut = "cut -f1,2,3,4,5,6,7,8,9,10 " + vcf2 + ".combine2 > " + outfile
     os.system(cut)
