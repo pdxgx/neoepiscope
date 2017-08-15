e05d7efede7c12dff0a331c61e970c95a07e1107
reidt03 <thompsre@ohsu.edu>
2017-08-15 10:39:02 -0700

    custom-naming for outfile

--- src/runhapcut2.py
+++ src/runhapcut2.py
@@ -1,8 +1,10 @@
 import os
 
-def runhapcut2(vcffile, bamfile):
-        script = "HapCUT2/build/extractHAIRS --indels 1 --bam " + bamfile + " --vcf " + vcffile + " --out fragment_file"
-        hap = "HapCUT2/build/HAPCUT2 --fragments fragment_file --vcf " + vcffile + " --output haplotype_output_file"
+def runhapcut2(vcffile, bamfile, outfile="Haplotype_output_file"):
+        script = "HapCUT2/build/extractHAIRS --indels 1 --bam " + bamfile + " --vcf " + vcffile + " --out " + outfile + ".frag"
+        hap = "HapCUT2/build/HAPCUT2 --fragments " + outfile + ".frag --vcf " + vcffile + " --output " + outfile
+        cleanup = "rm " + outfile + ".frag"
 
         os.system("bash -c '%s'" % script)
         os.system("bash -c '%s'" % hap)
+        os.system("bash -c '%s'" % cleanup)
