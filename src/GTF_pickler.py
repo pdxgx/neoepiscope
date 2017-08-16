230c7a94ccf2fb6462a12a2172aae9f634457f62
reidt03 <thompsre@ohsu.edu>
2017-08-15 18:14:36 -0700

    updated to streamline code
    also consolidates everything into cds_dict

--- src/GTF_pickler.py
+++ src/GTF_pickler.py
@@ -1,6 +1,6 @@
 import pickle
 import argparse
-import bisect
+import re
 
 
 parser = argparse.ArgumentParser()
@@ -14,55 +14,31 @@ args = parser.parse_args()
 
 my_file = open(args.gtf, "r") # ex: open("gencode.txt").read()
 
+#cds_dict[transcript_id]= [[start, stop, 1(if CDS)/0(if stop), reading frame, +/-], [start, stop, 1(if CDS)/0(if stop), reading frame, +/-], ...]
 cds_dict = {}
-cds_orf_dict = {}
-#exon_dict = {}
-#exon_orf_dict = {}
 for line in my_file:
     if not line or line[0] == '#': 
         continue
     tokens = line.strip().split('\t')
     if tokens[2] not in ["CDS", "start_codon", "stop_codon"]: 
         continue
+    ## not sure if we want to be using the start_codon at all or if this info is entirely redundant (?)
     transcript_id = re.sub(r'.*transcript_id \"([A-Z0-9._]+)\"[;].*', r'\1', tokens[8])
-    #version_in_id = read_info[1].find('.')
-    #if version_in_id == -1:
-    #    transcript_id = read_info[1][16:]
-    #else:
-    #    transcript_id = read_info[1][16:version_in_id]
     if transcript_id not in cds_dict:
         if tokens[2] == "CDS":
-                cds_dict[transcript_id] = [int(tokens[3]), int(tokens[4])]
+                cds_dict[transcript_id] = [[int(tokens[3]), int(tokens[4]), 1, tokens[6], int(tokens[7])]]
         elif tokens[2] == "stop_codon":
-                cds_dict[transcript_id] = [int(tokens[3]), int(tokens[4]), 0]
-        cds_orf_dict[transcript_id] = [tokens[6] + str(tokens[7])]
-    elif transcript_id in cds_dict and tokens[2] == "CDS":
-        insert_point = 2*bisect.bisect(cds_dict[transcript_id][0::2],
-                                       int(tokens[3]))
-        #I'm assuming there aren't two CDSs that start at the same point.
-        cds_dict[transcript_id] = (cds_dict[transcript_id][:insert_point] 
-                                    + [int(tokens[3]), int(tokens[4])] 
-                                    + cds_dict[transcript_id][insert_point:])
-        cds_orf_dict[transcript_id] = (cds_orf_dict[transcript_id][:insert_point//2]
-                                    + [tokens[6] + str(tokens[7])]
-                                    + cds_orf_dict[transcript_id][insert_point//2:])
-    elif transcript_id not in exon_dict and tokens[2] == "exon":
-        exon_dict[transcript_id] = [int(tokens[3]), int(tokens[4])]
-        exon_orf_dict[transcript_id] = [tokens[6] + str(tokens[7])]
-    elif transcript_id in exon_dict and tokens[2] == "exon":
-        insert_point = 2*bisect.bisect(exon_dict[transcript_id][0::2],
-                                       int(tokens[3]))
-        #I'm assuming there aren't two CDSs that start at the same point.
-        exon_dict[transcript_id] = (exon_dict[transcript_id][:insert_point] 
-                                    + [int(tokens[3]), int(tokens[4])] 
-                                    + exon_dict[transcript_id][insert_point:])
-        #Exons 
-        exon_orf_dict[transcript_id] = (exon_orf_dict[transcript_id][:insert_point//2]
-                                    + [tokens[6] + str(tokens[7])]
-                                    + exon_orf_dict[transcript_id][insert_point//2:])
+                cds_dict[transcript_id] = [[int(tokens[3]), int(tokens[4]), 0, tokens[6], int(tokens[7])]]
+    else:
+        if tokens[2] == "CDS":
+                cds_dict[transcript_id] = cds_dict[transcript_id].append([int(tokens[3]), int(tokens[4]), 1, tokens[6], int(tokens[7])])
+        elif tokens[2] == "stop_codon":
+                cds_dict[transcript_id] = cds_dict[transcript_id].append([int(tokens[3]), int(tokens[4]), 0, tokens[6], int(tokens[7])])
 
+for transcript_id in cds_dict:
+        cds_dict[transcript_id].sort(key=lambda x: x[0])
+                
     
-#@TODO: Don't forget to pickle the chrom_dict also!!!!!!!
 pickle_out = open(args.dump, "wb")
-pickle.dump([cds_dict, cds_orf_dict], pickle_out)
+pickle.dump(cds_dict, pickle_out)
 pickle_out.close()
