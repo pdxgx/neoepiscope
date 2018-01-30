b692ef3abb973e166a10dde8b61c67bf9fd36af2
Mary Wood <mary.a.wood.91@gmail.com>
2018-01-29 16:17:38 -0800

    removes extraneous files

--- src/combine.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-"""
-combine.py
-
-Combines HAPCUT2 output with 
-"""
--- src/tumorfilter.py
+++
@@ -1,5 +0,0 @@
-#Summary: Tumor filter will take results from previous blackbox in pipeline and eliminate reads that do not contain those mutated strings
-#Input: Tumor bam/sam file, mutated nucleotide k-mers
-#Process: Filter out reads from bam/sam that do not have the mutated nucleotide k-mers in alignment
-#Output: Only tumor reads that have mutations in list given
-
