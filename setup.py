159bf10d926ab908583eead10b72543f71044012
Mary Wood <wooma@ohsu.edu>
2020-05-04 10:30:28 -0700

    updates version numbers

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ class DownloadDependencies(Command):
 
 setup(
     name="neoepiscope",
-    version="0.3.8",
+    version="0.3.9",
     description="comprehensive neoepitope prediction software",
     long_description=(
         "neoepiscope is software for predicting neoepitopes "
